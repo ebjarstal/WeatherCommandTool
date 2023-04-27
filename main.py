ed647a4fdc57b12891f5ee0b04738c688a6afd54
ebj <ericbmousy@gmail.com>
2023-04-27 17:34:14 +0200

    Added while loop

--- main.py
+++ main.py
@@ -7,22 +7,25 @@ api_key = config.api_key
 base_url = "https://api.openweathermap.org/data/2.5/weather?q="
 
 if __name__ == "__main__":
-    city_name = input("Enter city name : ")
-    complete_url = base_url + city_name + "&appid=" + api_key
+    while True:
+        city_name = input("Enter city name ('q' to exit): ")
+        if city_name == "q":
+            break
 
-    response = requests.get(complete_url)
+        complete_url = base_url + city_name + "&appid=" + api_key
+        response = requests.get(complete_url)
+        data = response.json()
 
-    data = response.json()
+        # Checks if response contains weather data
+        if data["cod"] != "404":
+            weather = data["weather"][0]["description"]
+            temperature = round(data["main"]["temp"]-273.15, 0)
+            humidity = data["main"]["humidity"]
 
-    # Checks if response contains weather data
-    if data["cod"] != "404":
-        weather = data["weather"][0]["description"]
-        temperature = round(data["main"]["temp"]-273.15, 0)
-        humidity = data["main"]["humidity"]
+            print("Weather:", weather)
+            print("Temperature:", temperature, "C")
+            print("Humidity:", humidity, "%")
 
-        print("Weather:", weather)
-        print("Temperature:", temperature, "C")
-        print("Humidity:", humidity, "%")
-
-    else:
-        print(data["message"])
+        else:
+            print(data["message"])
+    exit(0)
